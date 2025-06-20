975b3bff68323b68c81e0b2df522158373655271
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-20 13:16:02 +0100

    feat: add startups data and implement GET endpoint to serve it

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
 const port = process.env.PORT;
+const startups = require("./data/data")
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.json(startups);
+});
 
 app.listen(port, () => console.log(`server connected on port: ${port}`));
