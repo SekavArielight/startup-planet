514f67fe007ca47eaef643afda22627fea54dfea
God'swill Sekav Apya <godswillapya@gmail.com>
2025-06-28 17:20:21 +0100

    feat: implement API routes for fetching startups data with filtering options

--- server.js
+++ server.js
@@ -1,69 +1,13 @@
 import express from "express";
 const port = process.env.PORT;
-import { startups } from "./data/data.js";
+import { apiRouter } from "./routes/apiRoutes.js";
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  let filteredData = startups;
+app.use("/api", apiRouter);
 
-  const { industry, country, continent, is_seeking_funding, has_mvp } =
-    req.query;
-
-  if (industry) {
-    filteredData = filteredData.filter(
-      (startup) => startup.industry.toLowerCase() === industry.toLowerCase()
-    );
-  }
-
-  if (country) {
-    filteredData = filteredData.filter(
-      (startup) => startup.country.toLowerCase() === country.toLowerCase()
-    );
-  }
-
-  if (continent) {
-    filteredData = filteredData.filter(
-      (startup) => startup.continent.toLowerCase() === continent.toLowerCase()
-    );
-  }
-
-  if (is_seeking_funding) {
-    filteredData = filteredData.filter(
-      (startup) =>
-        startup.is_seeking_funding ===
-        JSON.parse(is_seeking_funding.toLowerCase())
-    );
-  }
-
-  if (has_mvp) {
-    filteredData = filteredData.filter(
-      (startup) => startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
-    );
-  }
-
-  res.json(filteredData);
-});
-
-app.get("/api/:field/:term", (req, res) => {
-  const { field, term } = req.params;
-
-  const allowedFields = ["country", "continent", "industry"];
-
-  if (!allowedFields.includes(field)) {
-    return res
-      .status(400)
-      .json({
-        message:
-          "Search field not allowed. Please use only 'country', 'continent', 'industry'",
-      });
-  }
-
-  const filteredData = startups.filter(
-    (startup) => startup[field].toLowerCase() === term.toLowerCase()
-  );
-
-  res.json(filteredData);
-});
+// app.use((req, res) => {
+//   res.status(404).json({message: "Endpoint not found"});
+// });
 
 app.listen(port, () => console.log(`server connected on port: ${port}`));
